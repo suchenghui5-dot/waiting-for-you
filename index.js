d4f552a7310fa01115c5ee29ca116942d9a5c852
suchenghui5-dot <suchenghui5@gmail.com>
2026-04-28 00:27:40 +0800

    Refactor handler to async and enhance response
    Refactor handler to use async function and update response body.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html; charset=utf-8" },
-    body: "<h1>部署成功！waiting-for-you 运行中</h1>",
-  });
+// index.js
+exports.handler = async function(event, context, callback) {
+    callback(null, {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html; charset=utf-8'
+        },
+        body: `
+            <h1>✅ 部署成功！</h1>
+            <p>项目：waiting-for-you</p>
+            <p>GitHub：suchenghui5-dot</p>
+        `
+    });
 };
